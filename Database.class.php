ea5074ba761750446825162a7fdca2eaa88ec66e
Petr Knap <dev@petrknap.cz>
2014-11-24 15:11:36 +0100

    Database class 2.3.0

--- Database.class.php
+++ Database.class.php
@@ -6,7 +6,7 @@
  * @since    2013-06-03
  * @category DataStorage
  * @package  PetrKnap\Utils\DataStorage
- * @version  2.2.4
+ * @version  2.3.0
  * @license  https://github.com/petrknap/utils/blob/master/LICENSE MIT
  * @homepage http://dev.petrknap.cz/Database.class.php.html
  * @example  Database.example.php Basic usage example
@@ -22,6 +22,11 @@
  * @property bool AmICareful Are you careful?
  * @property string LastInsertId The ID of the last inserted row or sequence value
  *
+ * @change 2.3.0 Used `DatabaseException` instead of `\Exception`
+ * @change 2.3.0 Added method `BeginTransaction`["#method_BeginTransaction"]
+ * @change 2.3.0 Added method `Commit`["#method_Commit"]
+ * @change 2.3.0 Added method `RollBack`["#method_RollBack"]
+ * @change 2.3.0 Fixed minority error use cases
  * @change 2.2.4 Added method `lastInsertId`:[#method_lastInsertId]
  * @change 2.2.4 Added property `LastInsertId`:[#property_LastInsertId]
  * @change 2.2.3 Changed licensing from "MS-PL":[http://opensource.org/licenses/ms-pl.html] to "MIT":[https://github.com/petrknap/utils/blob/master/LICENSE]
@@ -43,6 +48,7 @@ class Database
     private $password = null;
     private $characterSet = null;
     private $amICareful = false;
+    private $hasActiveTransaction = false;
 
     /**
      * Supported databases
@@ -76,7 +82,7 @@ class Database
     /**
      * @param string $name
      * @return mixed
-     * @throws \Exception
+     * @throws DatabaseException
      */
     public function __get($name)
     {
@@ -100,10 +106,10 @@ class Database
             case "LastInsertId":
                 return $this->lastInsertId();
             case "Password":
-                throw new \Exception("Variable $" . $name . " is private.");
+                throw new DatabaseException("Variable $" . $name . " is private.", DatabaseException::SecurityException);
                 break;
             default:
-                throw new \Exception("Variable $" . $name . " not found.");
+                throw new DatabaseException("Variable $" . $name . " not found.");
                 break;
         }
     }
@@ -111,7 +117,7 @@ class Database
     /**
      * @param string $name
      * @param mixed $value
-     * @throws \Exception
+     * @throws DatabaseException
      */
     public function __set($name, $value)
     {
@@ -144,10 +150,10 @@ class Database
                 $this->roSet("amICareful", $value);
                 break;
             case "LastInsertId":
-                throw new \Exception("Variable $" . $name . " is readonly.");
+                throw new DatabaseException("Variable $" . $name . " is readonly.");
                 break;
             default:
-                throw new \Exception("Variable $" . $name . " not found.");
+                throw new DatabaseException("Variable $" . $name . " not found.");
                 break;
         }
     }
@@ -157,24 +163,45 @@ class Database
      *
      * @param string $name The name of attribute
      * @param mixed $value The value of attribute
-     * @throws \Exception
+     * @throws DatabaseException
      */
     private function roSet($name, $value)
     {
-        if ($this->{$name} != null)
-            throw new \Exception("Variable $" . $name . " is readonly (value " . $this->{$name} . ").");
-        else $this->{$name} = $value;
+        if ($this->{$name} != null) {
+            try {
+                throw new DatabaseException("Variable $" . $name . " is readonly (value " . serialize($this->{$name}) . ").");
+            } catch (\Exception $e) {
+                throw new DatabaseException("Variable $" . $name . " is readonly.");
+            }
+        } else {
+            $this->{$name} = $value;
+        }
+    }
+
+    /**
+     * @return \PDO Instance of PHP Data Object
+     * @throws DatabaseException
+     */
+    private function getPDO()
+    {
+        if (!$this->phpDataObject) {
+            throw new DatabaseException("Database is not connected.", DatabaseException::AccessException);
+        }
+        return $this->phpDataObject;
     }
 
     /**
      * Opens a new connection to a database
      *
      * @param string $dbName The name of your database
-     * @throws \Exception
+     * @return $this
+     * @throws DatabaseException
      */
     public function Connect($dbName = null)
     {
-        if ($this->phpDataObject) throw new \Exception("Only one connection to database per object is allowed.");
+        if ($this->phpDataObject) {
+            throw new DatabaseException("Only one connection to database per object is allowed.", DatabaseException::AccessException);
+        }
         if ($dbName !== null) $this->dbName = $dbName;
         switch ($this->type) {
             case self::TYPE_SQLite:
@@ -182,32 +209,41 @@ class Database
                 break;
             case self::TYPE_MySQL:
                 if (empty($this->dbName))
-                    throw new \Exception("Unspecified database name.");
+                    throw new DatabaseException("Unspecified database name.");
                 $dsn = "mysql:dbname=" . $this->dbName . ";host=" . $this->hostOrPath;
                 break;
             default:
-                throw new \Exception("Unsupported database type.");
+                throw new DatabaseException("Unsupported database type.", DatabaseException::AccessException);
                 break;
+        }
+        if (empty($this->hostOrPath)) {
+            throw new DatabaseException("Unspecified database host/file.", DatabaseException::AccessException);
         }
         try {
             $this->phpDataObject = new \PDO($dsn, $this->username, $this->password);
             $this->phpDataObject->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
         } catch (\PDOException $e) {
-            throw new \Exception("Could not connect: " . $e->getMessage());
+            throw new DatabaseException("Could not connect: " . $e->getMessage(), DatabaseException::AccessException, $e);
         }
         if ($this->characterSet) try {
-            self::Query("SET CHARACTER SET ?", $this->characterSet);
+            $this->Query("SET CHARACTER SET ?", $this->characterSet);
         } catch (\Exception $e) {
             $this->warnings[] = $e;
         }
+
+        return $this;
     }
 
     /**
      * Disables fail-safe mechanism for next query
+     *
+     * @return $this
      */
     public function IWillBeCareful()
     {
         $this->amICareful = true;
+
+        return $this;
     }
 
     /**
@@ -215,9 +251,11 @@ class Database
      *
      * @param string $name Name of the sequence object from which the ID should be returned
      * @return string Returns a string representing the row ID of the last row that was inserted into the database
+     * @throws DatabaseException
      */
-    public function lastInsertId($name = null) {
-        return $this->phpDataObject->lastInsertId($name);
+    public function lastInsertId($name = null)
+    {
+        return $this->getPDO()->lastInsertId($name);
     }
 
     /**
@@ -227,42 +265,133 @@ class Database
      *
      * @param string $query This must be a valid SQL statement for the target database server
      * @param mixed $params An array of values with as many elements as there are bound parameters in the SQL statement or single value
-     * @throws \Exception
      * @return \PDOStatement
+     * @throws DatabaseException
      */
     public function Query($query, $params = array())
     {
         if (!$this->amICareful) {
             if (preg_match("/^\\s*(delete|alter|drop) /i", $query)) {
-                throw new \Exception("You must be careful - call IWillBeCareful() method.");
+                throw new DatabaseException("You must be careful - call IWillBeCareful() method.", DatabaseException::SecurityException);
             }
         }
         $this->amICareful = false;
-        $result = $this->phpDataObject->prepare($query);
+
+        $statement = $this->getPDO()->prepare($query);
         if (!is_array($params)) $params = array($params);
-        $result->execute($params);
-        return $result;
+        $statement->execute($params);
+
+        return $statement;
+    }
+
+    /**
+     * Checks if instance has active transaction
+     *
+     * @param bool $value
+     * @throws DatabaseException
+     */
+    private function checkTransaction($value)
+    {
+        if ($this->hasActiveTransaction != $value) {
+            $message = "";
+            switch ($value) {
+                case true:
+                    $message = "You haven't active transaction - call BeginTransaction() method.";
+                    break;
+                case false:
+                    $message = "You have active transaction - call Commit() or RollBack() method.";
+                    break;
+
+            }
+            throw new DatabaseException($message, DatabaseException::AccessException);
+        }
+    }
+
+    /**
+     * Begins new transaction
+     *
+     * @return $this
+     * @throws DatabaseException
+     */
+    public function BeginTransaction()
+    {
+        $this->checkTransaction(false);
+        if (!$this->getPDO()->beginTransaction()) {
+            throw new DatabaseException("Couldn't begin transaction", DatabaseException::AccessException);
+        }
+        $this->hasActiveTransaction = true;
+
+        return $this;
+    }
+
+    /**
+     * Commits current transaction
+     *
+     * @return $this
+     * @throws DatabaseException
+     */
+    public function Commit()
+    {
+        $this->checkTransaction(true);
+        if (!$this->getPDO()->commit()) {
+            throw new DatabaseException("Couldn't commit transaction", DatabaseException::AccessException);
+        }
+        $this->hasActiveTransaction = false;
+
+        return $this;
+    }
+
+    /**
+     * Roll backs current transaction
+     *
+     * @return $this
+     * @throws DatabaseException
+     */
+    public function RollBack()
+    {
+        $this->checkTransaction(true);
+        if (!$this->getPDO()->rollBack()) {
+            throw new DatabaseException("Couldn't rollback transaction", DatabaseException::AccessException);
+        }
+        $this->hasActiveTransaction = false;
+
+        return $this;
     }
 
     /**
      * Returns an array that corresponds to the fetched row and moves the internal data pointer ahead
      *
-     * @param \PDOStatement $result The result resource that is being evaluated. This result comes from a call to self::Query().
-     * @param int $type             Controls how the next row will be returned to the caller - This value must be one of the Database::FETCH_* constants, defaults to Database::FETCH_BOTH
+     * @param \PDOStatement $statement The statement resource that is being evaluated. This statement comes from a call to Query().
+     * @param int $type Controls how the next row will be returned to the caller - This value must be one of the FETCH_* constants, defaults to FETCH_BOTH
      * @return array
      */
-    public function FetchArray($result, $type = self::FETCH_BOTH)
+    public function FetchArray($statement, $type = self::FETCH_BOTH)
     {
-        return $result->fetch($type);
+        return $statement->fetch($type);
     }
 
     /**
      * Closes the connection to the database
+     *
+     * @return $this
+     * @throws DatabaseException
      */
     public function Close()
     {
+        $this->checkTransaction(false);
         $this->phpDataObject = null;
+
+        return $this;
     }
+}
+
+class DatabaseException extends \Exception
+{
+    const
+        GenericException = 0,
+        AccessException = 1,
+        SecurityException = 2,
+        PDOException = 3;
 }
 
 #region Backward compatibility
---
+++ tests/DatabaseTest.class.php
@@ -0,0 +1,265 @@
+<?php
+
+use PetrKnap\Utils\DataStorage\Database;
+use PetrKnap\Utils\DataStorage\DatabaseException;
+
+require_once("../Database.class.php");
+
+class DatabaseTest extends PHPUnit_Framework_TestCase
+{
+    #region Empty tests
+    public function test_emptyType_connect()
+    {
+        try {
+            $db = new Database();
+            $db->Connect();
+            $this->fail("Empty type doesn't throw exception.");
+        } catch (DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+
+    }
+
+    public function test_emptyHostOrFile_connect()
+    {
+        try {
+            $db = new Database();
+            $db->Type = Database::TYPE_SQLite;
+            $db->Connect();
+            $this->fail("Empty hostOrFile doesn't throw exception.");
+        } catch (DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+    }
+
+    public function test_emptyPhpDataObject_query()
+    {
+        try {
+            $db = new Database();
+            $db->Query("");
+            $this->fail("Query on empty PhpDataObject doesn't throw exception.");
+        } catch (DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+    }
+
+    public function test_emptyPhpDataObject_lastInsertId()
+    {
+        try {
+            $db = new Database();
+            $db->lastInsertId();
+            $this->fail("lastInsertId on empty PhpDataObject doesn't throw exception.");
+        } catch (DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+    }
+    #endregion
+
+    #region Fail-safe mechanism tests
+    public function test_failSave_doubleConnection()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        try {
+            $db->Connect();
+            $this->fail("Second connection doesn't throw exception.");
+        } catch (DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+        $db->Close();
+    }
+
+    public function test_failSave_roSet()
+    {
+        $roVars = array(
+            "PhpDataObject",
+            "Type",
+            "Warnings",
+            "HostOrPath",
+            "Username",
+            "Password",
+            "DBName",
+            "CharacterSet",
+            "AmICareful",
+            "LastInsertId",
+            "InnerStatement"
+        );
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        foreach ($roVars as $roVar) {
+            try {
+                if (!$db->{$roVar}) {
+                    $db->{$roVar} = true;
+                }
+            } catch (DatabaseException $de) {
+                if($de->getCode() == DatabaseException::SecurityException) {
+                    continue;
+                }
+            }
+            try {
+                $db->{$roVar} = false;
+                $this->fail("{$roVar} = false doesn't throw exception.");
+            } catch (DatabaseException $de) {
+                $this->assertEquals(DatabaseException::GenericException, $de->getCode());
+            }
+        }
+    }
+
+    public function test_failSave_doubleTransaction()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect()->BeginTransaction();
+        try {
+            $db->BeginTransaction();
+            $this->fail("Second transaction doesn't throw exception.");
+        } catch(DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+        $db->RollBack()->Close();
+    }
+
+    public function test_failSave_commitWithoutTransaction()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        try {
+            $db->Commit();
+            $this->fail("Commit without transaction doesn't throw exception.");
+        } catch(DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+        $db->Close();
+    }
+
+    public function test_failSave_rollBackWithoutTransaction()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        try {
+            $db->RollBack();
+            $this->fail("Roll back without transaction doesn't throw exception.");
+        } catch(DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+        $db->Close();
+    }
+
+    public function test_failSave_closeWithTransaction()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect()->BeginTransaction();
+        try {
+            $db->Close();
+            $this->fail("Close with transaction doesn't throw exception.");
+        } catch(DatabaseException $de) {
+            $this->assertEquals(DatabaseException::AccessException, $de->getCode());
+        }
+        $db->RollBack()->Close();
+    }
+
+    public function test_failSave_dangerQuery()
+    {
+        $queries = array(
+            "DELETE FROM table_name",
+            "ALTER TABLE table_name ADD column_name INTEGER",
+            "DROP TABLE table_name"
+        );
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        $db->Query("CREATE TABLE IF NOT EXISTS table_name (id INTEGER PRIMARY KEY, data TEXT)");
+        foreach ($queries as $query) {
+            try {
+                $db->Query($query);
+                $this->fail("Danger query doesn't throw exception.");
+            } catch (DatabaseException $de) {
+                $this->assertEquals(DatabaseException::SecurityException, $de->getCode());
+            }
+        }
+        foreach ($queries as $query) {
+            $this->assertFalse($db->AmICareful);
+            $db->IWillBeCareful();
+            $this->assertTrue($db->AmICareful);
+            $db->Query($query);
+            $this->assertFalse($db->AmICareful);
+        }
+        $db->Close();
+    }
+    #endregion
+
+    #region Performance tests
+    public function test_performance()
+    {
+        $db = new Database();
+        $db->Type = Database::TYPE_SQLite;
+        $db->HostOrPath = ":memory:";
+        $db->Connect();
+        for ($i = 0; $i < 25; $i++) {
+
+            // Create table and fill it
+            $db->Query(
+                "CREATE TABLE IF NOT EXISTS table_{$i} (id INTEGER PRIMARY KEY, content TEXT)"
+            );
+            $k = 1;
+            $step = 500;
+            $stop = 100000;
+            while ($k < $stop) {
+                $keys = [];
+                $values = [];
+                for ($j = 0; $j < $step; $j++) {
+                    $keys[] = "(:d{$j})";
+                    $values["d{$j}"] = "d{$j}";
+                }
+                $db->Query(
+                    "INSERT INTO table_{$i} (content) VALUES " . join(", ", $keys),
+                    $keys
+                );
+                $k += $step;
+                $this->assertEquals($k - 1, $db->LastInsertId);
+            }
+
+            // Select many IDs, remove records by IDs and roll back changes
+            $db->BeginTransaction();
+            $rs = $db->Query("SELECT id FROM table_{$i} WHERE (id > 50 AND id < 101) OR (id > 250 AND id < 451)");
+            $ids = [];
+            while ($r = $db->FetchArray($rs)) {
+                $ids[] = $r["id"];
+            }
+            $db->IWillBeCareful();
+            $db->Query(
+                "DELETE FROM table_{$i} WHERE id IN (:ids)",
+                array(
+                    "ids" => join(", ", $ids)
+                )
+            );
+            $db->RollBack();
+
+            // Update records by content and commit changes
+            $db->BeginTransaction();
+            $db->Query(
+                "UPDATE table_{$i} SET content = 'd1 or d3' WHERE content LIKE :d1 OR content LIKE :d3",
+                array(
+                    "d1" => "d%1",
+                    "d3" => "d%3"
+                )
+            );
+            $db->Commit();
+
+        }
+        $db->Close();
+    }
+    #endregion
+}
