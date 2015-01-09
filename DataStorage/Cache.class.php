6fad62ebaf7dc19f0206b7340f4a9df26f7f942d
Petr Knap <dev@petrknap.cz>
2015-01-09 22:59:37 +0100

    Cache class 0.1

---
+++ DataStorage/Cache.class.php
@@ -0,0 +1,228 @@
+<?php namespace PetrKnap\Utils\DataStorage;
+
+/**
+ * Simple PHP class for caching your temporally static content
+ *
+ * This class saves data to APC if can, or to database trough Database object.
+ *
+ * @author   Petr Knap <dev@petrknap.cz>
+ * @since    2014-07-08
+ * @category DataStorage
+ * @package  PetrKnap\Utils\DataStorage
+ * @version  0.1
+ * @license  https://github.com/petrknap/utils/blob/master/LICENSE MIT
+ * @homepage http://dev.petrknap.cz/Database.class.php.html
+ * @example  Cache.example.php Basic usage example
+ * @property string Prefix The cache prefix
+ * @property bool DebugMode Debug mode flag
+ */
+class Cache
+{
+
+    /**
+     * @var bool Can use APC?
+     */
+    private $useAPC = false;
+
+    /**
+     * @var null|Database Alternative cache storage
+     */
+    private $dbCache = null;
+
+    /**
+     * @var string Prefix for this cache
+     */
+    private $prefix = "";
+
+    /**
+     * @var bool Is debug mode enabled?
+     */
+    private $debugMode = false;
+
+    /**
+     * SQL queries
+     */
+    const
+        CREATE_TABLE = "
+        CREATE TABLE IF NOT EXISTS Cache (
+            cache_key VARCHAR(200) UNIQUE NOT NULL,
+            cache_var TEXT,
+            cache_expire_at INTEGER
+        );",
+        INSERT = "
+        INSERT INTO Cache (cache_key, cache_var, cache_expire_at)
+            VALUES (:cache_key, :cache_var, :cache_expire_at)",
+        SELECT = "SELECT cache_var FROM Cache WHERE cache_key = ?",
+        DELETE = "DELETE FROM Cache WHERE cache_key = ?",
+        EXPIRE = "DELETE FROM Cache WHERE cache_expire_at < ? AND cache_expire_at IS NOT NULL",
+        CLEAN = "DELETE FROM Cache AND cache_key LIKE ?";
+
+    /**
+     * Creates new instance
+     *
+     * @param Database $DBCache Alternative cache storage
+     * @throws \Exception If couldn't connect to APC and $DBCache is null.
+     */
+    public function __construct($DBCache = null)
+    {
+        $this->useAPC = (extension_loaded('apc') && ini_get('apc.enabled'));
+
+        if ($DBCache !== null && !$this->useAPC) {
+            $this->dbCache = $DBCache;
+            if(!$this->dbCache->IsConnected) {
+                $this->dbCache->Connect();
+            }
+            $this->dbCache->Query(self::CREATE_TABLE);
+        } else if (!$this->useAPC) {
+            throw new \Exception("Couldn't use APC.");
+        }
+    }
+
+    /**
+     * Closes alternative cache storage
+     */
+    public function __destructor()
+    {
+        if ($this->dbCache) {
+            $this->dbCache->Close();
+        }
+    }
+
+    /**
+     * Caches a variable in the data store, only if it's not already stored
+     *
+     * After the ttl has passed, the stored variable will be expunged from the cache
+     * (on the next request). If no ttl is supplied (or if the ttl is 0), the value
+     * will persist until it is removed from the cache manually.
+     *
+     * @param string $key Store the variable using this name, keys are cache-unique
+     * @param mixed $var The variable to store
+     * @param int $ttl Time To Live; store var in the cache for ttl seconds
+     * @return bool
+     */
+    public function add($key, $var, $ttl = 0)
+    {
+        $key = "{$this->prefix}{$key}";
+        $return = false;
+        if ($this->useAPC) {
+            $return = \apc_add($key, $var, $ttl);
+        } else {
+            $this->dbCache->IWillBeCareful();
+            $this->dbCache->Query(self::EXPIRE, time());
+            try {
+                $this->dbCache->Query(
+                    self::INSERT,
+                    array(
+                        "cache_key" => $key,
+                        "cache_var" => \serialize($var),
+                        "cache_expire_at" => $ttl != 0 ? time() + $ttl : null
+                    )
+                );
+                $return = true;
+            } catch (\PDOException $ignored) {
+            }
+        }
+        return $return;
+    }
+
+    /**
+     * Fetch a stored variable from the cache
+     *
+     * @link http://php.net/manual/en/function.apc-fetch.php
+     * @param string $key The key used to store the value.
+     * @return mixed The stored variable on success; FALSE on failure.
+     */
+    public function get($key)
+    {
+        if($this->debugMode) {
+            return false;
+        }
+        $key = "{$this->prefix}{$key}";
+        $result = false;
+        if ($this->useAPC) {
+            $result = \apc_fetch($key);
+        } else {
+            $this->dbCache->IWillBeCareful();
+            $this->dbCache->Query(self::EXPIRE, time());
+            try {
+                $results = $this->dbCache->Query(self::SELECT, $key);
+                $result = $this->dbCache->FetchArray($results, Database::FETCH_ASSOC);
+                if ($result) $result = \unserialize($result["cache_var"]);
+            } catch (\PDOException $ignored) {
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Removes a stored variable from the cache
+     *
+     * @param string $key The key used to store the value.
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
+    public function del($key)
+    {
+        $key = "{$this->prefix}{$key}";
+        $return = false;
+        if ($this->useAPC) {
+            $return = \apc_delete($key);
+        } else {
+            $this->dbCache->IWillBeCareful();
+            $this->dbCache->Query(self::EXPIRE, time());
+            try {
+                $this->dbCache->IWillBeCareful();
+                $this->dbCache->Query(self::DELETE, $key);
+                $return = true;
+            } catch (\PDOException $ignored) {
+            }
+        }
+        return $return;
+    }
+
+    /**
+     * Clears the APC cache
+     *
+     * The user cache will be cleared.
+     *
+     * @return bool Returns TRUE on success or FALSE on failure.
+     */
+    public function clear()
+    {
+        $return = false;
+        if ($this->useAPC) {
+            $return = \apc_clear_cache("user");
+        } else {
+            try {
+                $this->dbCache->IWillBeCareful();
+                $this->dbCache->Query(self::CLEAN, "{$this->prefix}%");
+                $return = true;
+            } catch (\PDOException $ignored) {
+            }
+        }
+        return $return;
+    }
+
+    public function __get($name) {
+        switch($name) {
+            case "DebugMode":
+                return $this->debugMode;
+            case "Prefix":
+                return $this->prefix;
+            default:
+                throw new \Exception("Variable $" . $name . " not found.");
+        }
+    }
+
+    public function __set($name, $value) {
+        switch($name) {
+            case "DebugMode":
+                $this->debugMode = $value;
+                break;
+            case "Prefix":
+                $this->prefix = $value;
+                break;
+            default:
+                throw new \Exception("Variable $" . $name . " not found.");
+        }
+    }
+}
---
+++ test/DataStorage/CacheTest.class.php
@@ -0,0 +1,101 @@
+<?php
+
+use PetrKnap\Utils\DataStorage\Database;
+use PetrKnap\Utils\DataStorage\Cache;
+
+require_once(__DIR__ . "/../../DataStorage/Database.class.php");
+require_once(__DIR__ . "/../../DataStorage/Cache.class.php");
+
+class CacheTest extends PHPUnit_Framework_TestCase
+{
+    private $database;
+
+    const CACHE_KEY = "content", CACHE_PREFIX_A = "A", CACHE_PREFIX_B = "B";
+
+    public function setUp() {
+        $this->database = new Database();
+        $this->database->Type = Database::TYPE_SQLite;
+        $this->database->HostOrPath = ":memory:";
+        $this->database->CharacterSet = "UTF8";
+    }
+
+    public function testSimpleCachingWorks() {
+        $count = 0;
+        $counter = function() use (&$count) {
+            return ++$count;
+        };
+
+        $cache = new Cache($this->database);
+
+        $this->assertFalse($cache->get(self::CACHE_KEY));
+
+        for($i = 0; $i < 100; $i++) {
+            $content = $cache->get(self::CACHE_KEY);
+            if(!$content) {
+                $content = $counter();
+                $cache->add(self::CACHE_KEY, $content);
+            }
+            $this->assertEquals(1, $content);
+        }
+    }
+
+    public function testDebugModeWorks() {
+        $cache = new Cache($this->database);
+
+        $cache->add(self::CACHE_KEY, "debug");
+
+        $this->assertFalse($cache->get(self::CACHE_KEY));
+    }
+
+    public function testItemCanBeDeletedFromCacheByUser() {
+        $cache = new Cache($this->database);
+        $cache->add(self::CACHE_KEY, "remove me");
+
+        $this->assertEquals("remove me", $cache->get(self::CACHE_KEY));
+
+        $cache->del(self::CACHE_KEY);
+
+        $this->assertFalse($cache->get(self::CACHE_KEY));
+    }
+
+    public function testItemCanByDeletedByTime() {
+        $cache = new Cache($this->database);
+
+        $content = "remove me after 1 second";
+
+        $cache->add(self::CACHE_KEY, $content, 2);
+
+        $this->assertEquals($content, $cache->get(self::CACHE_KEY));
+
+        sleep(4);
+
+        $this->assertFalse($cache->get(self::CACHE_KEY));
+    }
+
+    public function testPrefixesWorksFine() {
+        $cache = new Cache($this->database);
+
+        $cache->Prefix = self::CACHE_PREFIX_A;
+        $cache->add(self::CACHE_KEY, "A");
+
+        $cache->Prefix = self::CACHE_PREFIX_B;
+        $this->assertFalse($cache->get(self::CACHE_KEY));
+        $cache->add(self::CACHE_KEY, "B");
+
+        $cache->Prefix = self::CACHE_PREFIX_A;
+        $this->assertEquals("A", $cache->get(self::CACHE_KEY));
+
+        $cache->Prefix = self::CACHE_PREFIX_B;
+        $this->assertEquals("B", $cache->get(self::CACHE_KEY));
+    }
+
+    public function testTwoInstancesOnOneDatabaseWorksAsOneInstance() {
+        $cacheA = new Cache($this->database);
+        $cacheB = new Cache($this->database);
+
+        $content = "content";
+        $cacheA->add(self::CACHE_KEY, $content);
+        $this->assertSame($content, $cacheB->get(self::CACHE_KEY));
+    }
+
+}
