49325904747aa26c925aabd8775836d947a880bb
Petr Knap <dev@petrknap.cz>
2015-01-10 11:29:57 +0100

    HttpResponseTest was moved

---
+++ Network/HttpResponseTest.php
@@ -0,0 +1,27 @@
+<?php namespace PetrKnap\Utils\Network;
+
+class HttpResponseTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var HttpResponse
+     */
+    private $response;
+
+    public function setUp() {
+        $this->response = new HttpResponse();
+    }
+
+    public function testTemporaryRedirectionWorks() {
+        $this->response->redirect("http://dev.petrknap.cz/");
+
+        $this->assertArrayHasKey("Location", $this->response->getHeaders());
+        $this->assertArrayNotHasKey("HTTP/1.1 301 Moved Permanently", $this->response->getHeaders());
+    }
+
+    public function testPermanentRedirectionWorks() {
+        $this->response->redirect("http://dev.petrknap.cz/");
+
+        $this->assertArrayHasKey("Location", $this->response->getHeaders());
+        $this->assertArrayNotHasKey("HTTP/1.1 301 Moved Permanently", $this->response->getHeaders());
+    }
+}
